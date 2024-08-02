2fbf4d81d5513554935361be13d1a72acccd1bcd
EzzatEsam <ezzatesam1214@gmail.com>
2024-08-03 00:03:14 +0300

    fixed empty header

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -4,7 +4,7 @@ import { MyData } from "@/data/data";
 const xpPage: React.FC = () => {
   return (
     <div className="m-8 mx-auto max-w-4xl rounded-lg bg-white p-6 shadow-lg dark:bg-gray-800 dark:shadow-gray-700">
-      <h1 className="mb-6 text-3xl font-semibold text-gray-900 dark:text-gray-100"></h1>
+      <h1 className="mb-6 text-3xl font-semibold text-gray-900 dark:text-gray-100">{" "}</h1>
       <Timeline events={MyData.myExperience} />
     </div>
   );
